d10cf374569358c5b83514718f5b46c6720b4d10
AaYsAuNkAaMI <100006221+AaYsAuNkAaMI@users.noreply.github.com>
2024-02-15 05:15:52 +0000

    Copilot third  commit

--- comments.js
+++ comments.js
@@ -1,91 +1,45 @@
-// create web server for comment
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+//create web server
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
 
-app.use(bodyParser.json());
-
-app.post('/comment', function(req, res) {
-    console.log(req.body);
-    res.send(req.body);
-});
-
-var server = app.listen(8081, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
-```
-
-## 4.2.3. Run server
-
-Run the server by using the following command:
-
-```
-$ node comment.js
-```
-
-## 4.2.4. Test the server
-
-Use the following command to test the server:
-
-```
-$ curl -X POST -H "Content-Type: application/json" -d '{"name": "John", "age": 30}' http://localhost:8081/comment
-```
-
-## 4.2.5. Result
-
-The server will print the following result:
-
-```
-{ name: 'John', age: 30 }
-```
-
-# 4.3. RESTful API
-
-RESTful API is an architectural style for an application programming interface (API) that uses HTTP requests to GET, PUT, POST and DELETE data.
-
-## 4.3.1. Create a RESTful API server
-
-Create a file named `restful.js` with the following content:
-
-```js
-// Path: restful.js
-// create web server for RESTful API
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
-app.get('/user', function(req, res) {
-    res.send('GET request to the homepage');
-});
-
-app.post('/user', function(req, res) {
-    res.send('POST request to the homepage');
-});
-
-app.put('/user', function(req, res) {
-    res.send('PUT request to the homepage');
-});
-
-app.delete('/user', function(req, res) {
-    res.send('DELETE request to the homepage');
-});
-
-var server = app.listen(8082, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
-```
-
-## 4.3.2. Run server
-
-Run the server by using the following command:
-
-```
- 
+http.createServer(function(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    var ext = path.extname(pathname);
+    var type = '';
+    var data = '';
+    switch (ext) {
+        case '.html':
+            type = 'text/html';
+            data = fs.readFileSync(__dirname + pathname, 'utf-8');
+            break;
+        case '.js':
+            type = 'text/javascript';
+            data = fs.readFileSync(__dirname + pathname, 'utf-8');
+            break;
+        case '.css':
+            type = 'text/css';
+            data = fs.readFileSync(__dirname + pathname, 'utf-8');
+            break;
+        case '.jpg':
+            type = 'image/jpeg';
+            data = fs.readFileSync(__dirname + pathname);
+            break;
+        case '.png':
+            type = 'image/png';
+            data = fs.readFileSync(__dirname + pathname);
+            break;
+        case '.gif':
+            type = 'image/gif';
+            data = fs.readFileSync(__dirname + pathname);
+            break;
+        default:
+            type = 'text/html';
+            data = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+            break;
+    }
+    res.writeHead(200, {'Content-Type': type});
+    res.write(data);
+    res.end();
+}).listen(1337, '
